cd52c84cb4de8efdafd66838d119a2639d021699
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2018-12-18 14:03:08 +0800

    清除一些不要的代码

--- models/user.js
+++ models/user.js
@@ -16,40 +16,6 @@ var url = 'mongodb://localhost:27017';
 
 var Mongpromise = MongoClient.connect(url, {useNewUrlParser: true});
 
-
-// //存储用户信息
-// User.prototype.save = function(callback) {
-//   //要存入数据库的用户文档
-//   var user = {
-//       name: this.name,
-//       password: this.password,
-//       email: this.email
-//   };
-//   //打开数据库
-//   mongodb.open(function (err, db) {
-//     if (err) {
-//       return callback(err);//错误，返回 err 信息
-//     }
-//     //读取 users 集合
-//     db.collection('users', function (err, collection) {
-//       if (err) {
-//         mongodb.close();
-//         return callback(err);//错误，返回 err 信息
-//       }
-//       //将用户数据插入 users 集合
-//       collection.insert(user, {
-//         safe: true
-//       }, function (err, user) {
-//         mongodb.close();
-//         if (err) {
-//           return callback(err);//错误，返回 err 信息
-//         }
-//         callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
-//       });
-//     });
-//   });
-// };
-
 User.prototype.save = function(callback) {
   var user = {
     name: this.name,
@@ -77,27 +43,6 @@ User.prototype.save = function(callback) {
    });
   })
 }
-
-//读取用户信息
-// User.get = function(name, callback) {
-//     //读取 users 集合
-//     mongodb.collection('users', function (err, collection) {
-//       if (err) {
-//         mongodb.close();
-//         return callback(err);//错误，返回 err 信息
-//       }
-//       //查找用户名（name键）值为 name 一个文档
-//       collection.findOne({
-//         name: name
-//       }, function (err, user) {
-//         mongodb.close();
-//         if (err) {
-//           return callback(err);//失败！返回 err 信息
-//         }
-//         callback(null, user);//成功！返回查询的用户信息
-//       });
-//     });
-// };
 
 User.prototype.get = function(callback) {
   Mongpromise.then((mongodb) => {
--- routes/index.js
+++ routes/index.js
@@ -17,44 +17,6 @@ router.get('/reg', function(req, res, next) {
   res.render('reg', { title: 'myblog'});
 })
 
-// router.post('/reg', function (req, res) {
-//   var name = req.body.name,
-//     password = req.body.password,
-//     password_re = req.body['password-repeat'];
-//     //检验用户两次输入的密码是否一致
-//     if (password_re != password) {
-//       req.flash('error', '两次输入的密码不一致！');
-//       return res.redirect('/reg');  //返回注册页
-//     }
-//     //生成密码的md5值
-//     var md5 = crypto.createHash('md5'),
-//       password = md5.update(req.body.password).digest('hex'); //update什么意思?
-//     var newUser = new User({
-//       name: name,
-//       password: password,
-//       email: req.body.email
-//     });
-//
-//     //检测用户名是否已经存在
-//     User.get(newUser.name, function(err, user) {
-//       if (err) {
-//         req.flash('error', '用户已经存在！');
-//         console.log('error', '用户已经存在！')
-//         return res.redirect('/reg');
-//       }
-//       newUser.save(function (err, user) {
-//         if (err) {
-//           req.flash('error', err);
-//           console.log('注册失败');
-//           return res.redirect('/reg')
-//         }
-//         req.session.user = user;
-//         req.flash('success', '注册成功！');
-//         res.redirect('/');
-//       })
-//     })
-//
-// })
 
 router.post('/reg', function(req, res) {
   var name = req.body.name,
