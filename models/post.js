11351ed913b0126d0c3c0548c23e67e6adf14a7e
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2018-12-20 22:06:04 +0800

    可以从sqlserver中读取文章了，但是还要解决 中文乱码，需要排序

--- models/post.js
+++ models/post.js
@@ -9,6 +9,8 @@ const config = {
   database: 'luntan',
   port: 1443
 }
+var poolPromise = mssql.connect(config);
+
 function Post(name, title, post) {
   this.name = name;
   this.title = title;
@@ -37,18 +39,19 @@ Post.prototype.save = function(callback) {
   };
   //使用mssql存储
   console.log('这里值了吧')
-  var poolPromise = mssql.connect(config);
   console.log(poolPromise)
   poolPromise.then(function(pool) {
-    return pool.request()
+    pool.request()
       .input('name', mssql.Char, post.name)
       .input('title', mssql.Char, post.title)
       .input('content', mssql.Text, post.post)
       .query('insert into posts(Title, Content, AuthorName, CreateDate, LastDate) values(@title, @content, @name, getdate(), getdate())')
-  }).then(result => {
-    console.log(result)
-    console.log('执行了吗');
+      .then(function(result){
+        console.log(result)
+        console.log('执行了吗');
+      })
   })
+
 
   //打开数据库
   mongodbClientPromise.then(function(client) {
@@ -74,26 +77,38 @@ Post.prototype.save = function(callback) {
 
 Post.get = function(name, callback) {
   //打开数据库
-  mongodbClientPromise.then(function(client) {
-    var db = client.db('blog');
-    db.collection('posts', function(err, collection) {
-      if (err) {
-        callback(err)
-      } else {
-        var query = {};
-        if (name) {
-          query.name = name;
-        }
-        collection.find(query).sort({
-          time: -1
-        }).toArray(function (err, docs) {
-          if(err) {
-            callback(err);
-          } else {
-            callback(null, docs);
-          }
-        })
-      }
+  // mongodbClientPromise.then(function(client) {
+  //   var db = client.db('blog');
+  //   db.collection('posts', function(err, collection) {
+  //     if (err) {
+  //       callback(err)
+  //     } else {
+  //       var query = {};
+  //       if (name) {
+  //         query.name = name;
+  //       }
+  //       collection.find(query).sort({
+  //         time: -1
+  //       }).toArray(function (err, docs) {
+  //         if(err) {
+  //           callback(err);
+  //         } else {
+  //           callback(null, docs);
+  //         }
+  //       })
+  //     }
+  //   })
+  // })
+  poolPromise.then(function(pool) {
+    pool.request()
+    .query('select * from posts')
+    .then(function(recordset) {
+      console.dir(recordset);
+      callback(null, recordset.recordset);
+    })
+    .catch(function(err) {
+      console.log(err);
+      callback(err);
     })
   })
 }
